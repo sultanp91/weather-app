b72f3811a7e41d2c23dca12c8ba662e26dca40cc
Sultan <sultanp@gmail.com>
2021-02-06 01:10:24 +0000

    weather data will now update the DOM

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 const locationinput = document.querySelector("#cityinput");
 const locationsubmit = document.querySelector("#submit");
 
+const temp = document.querySelector("#temp");
+const tempFeel = document.querySelector("#tempfeel");
+const condition = document.querySelector("#condition");
+const city = document.querySelector("#city");
+const img = document.querySelector("#icon");
 
+// Factory function to create objects with selected weather data
 
 const dataFactory = (temp, tempfeel, condition, icon, city) => {
     
     return { temp, tempfeel, condition, icon, city }
 }
 
-
 async function getWeatherData (city) {
 
     const request = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=d814b06e7180432ffac829ff0f153fb9`, { mode: "cors" })
+    
     const data = await request.json();
    
     const obj = await dataFactory(data.main.temp, data.main.feels_like, data.weather[0].description, 
@@ -21,8 +27,18 @@ async function getWeatherData (city) {
 }
 
 locationsubmit.addEventListener("click", async () => {
+   
     let city = locationinput.value;
 
     let data = await getWeatherData(city);
-      
+
+    updateDOM(data);
 })
+
+const updateDOM = (data) => {
+    temp.textContent = data.temp;
+    tempfeel.textContent = data.tempfeel;
+    condition.textContent = data.condition;
+    city.textContent = data.city;
+    img.src = `http://openweathermap.org/img/wn/${data.icon}@2x.png`;
+}
