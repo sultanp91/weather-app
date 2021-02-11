fadef7098fb75b9b91144773a8072fc369130e94
Sultan <sultanp@gmail.com>
2021-02-11 01:40:10 +0000

    linting and re-organising code

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 // DOM Elements
-const locationinput = document.querySelector("#cityinput");
-const locationsubmit = document.querySelector("#submit");
-const locationIcon = document.querySelector("#location-logo");
+const locationinput = document.querySelector('#cityinput');
+const locationsubmit = document.querySelector('#submit');
+const locationIcon = document.querySelector('#location-logo');
 const toggleButton = document.querySelector('#toggle');
 
-const temp = document.querySelector("#temp");
-const tempFeel = document.querySelector("#tempfeel");
-const condition = document.querySelector("#condition");
-const city = document.querySelector("#city");
-const img = document.querySelector("#icon");
+const temp = document.querySelector('#temp');
+const tempFeel = document.querySelector('#tempfeel');
+const condition = document.querySelector('#condition');
+const city = document.querySelector('#city');
+const img = document.querySelector('#icon');
 
 // Object which will contain weather data to be rendered onto the DOM
 
@@ -19,103 +19,120 @@ let displayFahrenheit = false;
 // Factory function to create objects with selected weather data
 
 const dataFactory = (temp, tempfeel, condition, icon, city) => {
-    
-    const getTempCelsius = Math.round(temp);
+  const getTempCelsius = Math.round(temp);
 
-    const getTempFeelCelsius = Math.round(tempfeel);
+  const getTempFeelCelsius = Math.round(tempfeel);
 
-    const getTempFahrenheit = Math.round((temp * 1.8) + 32);
+  const getTempFahrenheit = Math.round(temp * 1.8 + 32);
 
-    const getTempFeelFahrenheit = Math.round((tempfeel * 1.8) + 32);
+  const getTempFeelFahrenheit = Math.round(tempfeel * 1.8 + 32);
 
-    return { getTempCelsius, getTempFeelCelsius, getTempFahrenheit, getTempFeelFahrenheit, condition, icon, city }
-}
-
-async function getWeatherData (city) {
-
-    const request = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=d814b06e7180432ffac829ff0f153fb9`, { mode: "cors" })
-    
-    weatherDataRequest = await request.json();
-   
-    console.log(weatherData)
-    
-    weatherData = await dataFactory(weatherDataRequest.main.temp, weatherDataRequest.main.feels_like, weatherDataRequest.weather[0].main, 
-        weatherDataRequest.weather[0].icon, weatherDataRequest.name);
-
-}
-
-locationsubmit.addEventListener("click", async (e) => {
-   
-    e.preventDefault()
-
-    const city = locationinput.value;
-
-    await getWeatherData(city);
-
-    updateDom();
-})
+  return {
+    getTempCelsius,
+    getTempFeelCelsius,
+    getTempFahrenheit,
+    getTempFeelFahrenheit,
+    condition,
+    icon,
+    city,
+  };
+};
 
 const updateDomCelsius = () => {
-    temp.textContent = `${weatherData.getTempCelsius}  \u00B0C`;
-    tempfeel.textContent = `${weatherData.getTempFeelCelsius} \u00B0C`;
-    condition.textContent = weatherData.condition;
-    city.textContent = weatherData.city;
-    img.src = `http://openweathermap.org/img/wn/${weatherData.icon}@2x.png`;
-}
+  temp.textContent = `${weatherData.getTempCelsius}  \u00B0C`;
+  tempfeel.textContent = `${weatherData.getTempFeelCelsius} \u00B0C`;
+  condition.textContent = weatherData.condition;
+  city.textContent = weatherData.city;
+  img.src = `http://openweathermap.org/img/wn/${weatherData.icon}@2x.png`;
+};
 
 const updateDomFahrenheit = () => {
-    temp.textContent = `${weatherData.getTempFahrenheit}  \u00B0F`;
-    tempfeel.textContent = `${weatherData.getTempFeelFahrenheit} \u00B0F`;
-    condition.textContent = weatherData.condition;
-    city.textContent = weatherData.city;
-    img.src = `http://openweathermap.org/img/wn/${weatherData.icon}@2x.png`;
-}
+  temp.textContent = `${weatherData.getTempFahrenheit}  \u00B0F`;
+  tempfeel.textContent = `${weatherData.getTempFeelFahrenheit} \u00B0F`;
+  condition.textContent = weatherData.condition;
+  city.textContent = weatherData.city;
+  img.src = `http://openweathermap.org/img/wn/${weatherData.icon}@2x.png`;
+};
 
 const updateDom = () => {
-    if (displayCelsius === true) {
-        updateDomCelsius();
-    } else if (displayFahrenheit === true){
-        updateDomFahrenheit();
-    }
+  if (displayCelsius === true) {
+    updateDomCelsius();
+  } else if (displayFahrenheit === true) {
+    updateDomFahrenheit();
+  }
+};
+
+async function getWeatherData(city) {
+  try {
+    const request = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=d814b06e7180432ffac829ff0f153fb9`,
+      { mode: 'cors' },
+    );
+
+    const weatherDataRequest = await request.json();
+
+    weatherData = await dataFactory(
+      weatherDataRequest.main.temp,
+      weatherDataRequest.main.feels_like,
+      weatherDataRequest.weather[0].main,
+      weatherDataRequest.weather[0].icon,
+      weatherDataRequest.name,
+    );
+  } catch (error) {
+    console.log(error);
+  }
 }
+
+locationsubmit.addEventListener('click', async (e) => {
+  e.preventDefault();
+
+  const cityInput = locationinput.value;
+
+  await getWeatherData(cityInput);
+
+  updateDom();
+});
 
 const toggleUnits = () => {
-    if(displayCelsius === true) {
-        displayCelsius = false;
-        displayFahrenheit = true;
-        updateDom()
-    } 
-    else if(displayFahrenheit === true){
-        displayFahrenheit = false;
-        displayCelsius = true;
-        updateDom()
-    }
-}
+  if (displayCelsius === true) {
+    displayCelsius = false;
+    displayFahrenheit = true;
+    updateDom();
+  } else if (displayFahrenheit === true) {
+    displayFahrenheit = false;
+    displayCelsius = true;
+    updateDom();
+  }
+};
 
 const userLocationInput = async function () {
-    
-        async function success(result) {
-        const coordinates = result.coords;
-        const latitude = coordinates.latitude;
-        const longitude = coordinates.longitude;
+  async function success(result) {
+    const coordinates = result.coords;
+    const { latitude } = coordinates;
+    const { longitude } = coordinates;
 
-        const locationRequest = await fetch(`https://eu1.locationiq.com/v1/reverse.php?key=pk.74f454060c7a9f8b245dd7bf24c08d88&lat=${latitude}&lon=${longitude}&format=json`)
-        const cityData = await locationRequest.json()
-        
-        await getWeatherData(cityData.address.city);
+    try {
+      const locationRequest = await fetch(
+        `https://eu1.locationiq.com/v1/reverse.php?key=pk.74f454060c7a9f8b245dd7bf24c08d88&lat=${latitude}&lon=${longitude}&format=json`,
+      );
+      const cityData = await locationRequest.json();
 
-        updateDom();
-
+      await getWeatherData(cityData.address.city);
+    } catch (error) {
+      console.log(error);
     }
-    navigator.geolocation.getCurrentPosition(success)
-}
 
-  window.addEventListener('load', userLocationInput);
+    updateDom();
+  }
+  navigator.geolocation.getCurrentPosition(success);
+};
 
-  locationIcon.addEventListener('click', userLocationInput)
+window.addEventListener('load', userLocationInput);
 
-  toggleButton.addEventListener("click", (e) => {
-        e.preventDefault()
+locationIcon.addEventListener('click', userLocationInput);
 
-        toggleUnits();
-  });
+toggleButton.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  toggleUnits();
+});
